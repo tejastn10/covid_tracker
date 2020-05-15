eff409c2d7b0996c93f8f5f645b6f5746b054b4b
tejastn10 <tejastn10@gmail.com>
2020-05-15 12:30:05 +0530

    Data fetched from Api

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,26 @@ import React from "react";
 
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
+import { fetchData } from "./api";
 class App extends React.Component {
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const fetcedData = await fetchData();
+
+    console.log(fetcedData);
+    this.setState({ data: fetcedData });
+  }
   render() {
+    const { data } = this.state;
+
     return (
       <div>
         <h1>App</h1>
         <div className={styles.container}>
-          <Cards />
+          <Cards {...data} />
           <CountryPicker />
           <Chart />
         </div>
